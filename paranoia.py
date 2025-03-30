f65838cef6fbe9bd8e83e3eb3887fc834b601d75
olivcho <olivercho007@gmail.com>
2025-03-30 10:45:12 -0700

    randomly assign targets

--- paranoia.py
+++ paranoia.py
@@ -1,1 +1,26 @@
-# scraper
+import pandas as pd
+import numpy as np
+import requests
+import json
+import os
+
+with open("student_directory.json", "r") as f:
+    student_directory = json.load(f)
+
+grade_input = str(input("Please enter the grade you want to filter by (e.g., '12'): "))
+senior_data = []
+
+for student in student_directory:
+    if student["GradeDisplay"] == grade_input:
+        senior_data.append({
+            "Name": f"{student['FirstName']} {student['LastName']}",
+            "Email": student["Email"]
+        })
+
+df = pd.DataFrame(senior_data)
+print("Data successfully collected from student directory.")
+print(f"First 5 entries: \n{df.head()} \n")
+
+# Create third column for Paranoia targets
+df['Paranoia Target'] = np.random.permutation(df['Name'].values)
+print(f"First 5 entries with Paranoia Target: \n{df.head()} \n")
